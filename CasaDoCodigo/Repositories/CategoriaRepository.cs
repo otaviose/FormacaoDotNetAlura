0cc0418e84c828207be51516cca7b97c674e0b5f
otaviose <nicolassilva114@hotmail.com>
2018-07-21 21:22:07 -0300

    Initial

---
+++ CasaDoCodigo/Repositories/CategoriaRepository.cs
@@ -0,0 +1,33 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface ICategoriaRepository
+    {
+        Categoria Inserir(string nome);
+    }
+
+    public class CategoriaRepository : BaseRepository<Categoria>, ICategoriaRepository
+    {
+        public CategoriaRepository(ApplicationContext context) : base(context)
+        {
+
+        }
+
+        public Categoria Inserir(string nome)
+        {
+            var categoria = dbSet.FirstOrDefault(c => c.Nome == nome);
+            if (categoria == null)
+            {
+                categoria = new Categoria { Nome = nome };
+                dbSet.Add(categoria);
+                contexto.SaveChanges();
+            }
+            return categoria;
+        }
+    }
+}
---
+++ CasaDoCodigo/Repositories/ProdutoRepository.cs
@@ -0,0 +1,65 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class ProdutoRepository : BaseRepository<Produto>, IProdutoRepository
+    {
+        private readonly ICategoriaRepository categoriaRepository;
+
+        public ProdutoRepository(ApplicationContext contexto,
+            ICategoriaRepository categoriaRepository) : base(contexto)
+        {
+            this.categoriaRepository = categoriaRepository;
+        }
+
+        public IList<Produto> GetProdutos()
+        {
+            return dbSet
+                .Include(p => p.Categoria)
+                .ToList();
+        }
+
+        public IList<Produto> GetProdutos(string pesquisa)
+        {
+            var produtos = dbSet
+                .Include(p => p.Categoria)
+                .ToList();
+
+            var produtosFiltrados = produtos
+                .Where(p => p.Nome.ToUpper().Contains(pesquisa) || p.Categoria.Nome.ToUpper().Contains(pesquisa))
+                .ToList();
+
+            if (produtosFiltrados.Count > 0)
+                return produtosFiltrados;
+
+            return produtos;
+        }
+
+        public void SaveProdutos(List<Livro> livros)
+        {
+            foreach (var livro in livros)
+            {
+                if (!dbSet.Where(p => p.Codigo == livro.Codigo).Any())
+                {
+                    var categoria = categoriaRepository.Inserir(livro.Categoria);
+                    dbSet.Add(new Produto(livro.Codigo, livro.Nome, livro.Preco, categoria));
+                }
+            }
+            contexto.SaveChanges();
+        }
+    }
+
+    public class Livro
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public string Subcategoria { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
